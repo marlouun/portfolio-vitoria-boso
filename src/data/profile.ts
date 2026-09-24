5be04b64a341780e5d4f3464f36eaa743f0236c5
Marlon Albino <marlon.silva@unifebe.edu.br>
2026-09-24 20:11:35 -0300

    content: adiciona WhatsApp da Vitoria

--- src/data/profile.ts
+++ src/data/profile.ts
@@ -9,6 +9,8 @@ export const profile = {
   location: 'Brasil',
   instagram: 'https://www.instagram.com/_framesby.vi',
   highlights: 'https://www.instagram.com/stories/highlights/18084929305949392/',
+  whatsapp:
+    'https://wa.me/554891842048?text=Oi%2C%20Vit%C3%B3ria%21%20Vi%20seu%20portf%C3%B3lio%20e%20gostaria%20de%20conversar%20sobre%20um%20ensaio%20fotogr%C3%A1fico.%20Pode%20me%20passar%20mais%20informa%C3%A7%C3%B5es%3F',
   profileImage: `${imageBase}images/vitoria/igreja/IMG_0614.JPG.jpeg`,
   heroImage: `${imageBase}images/vitoria/igreja/IMG_0606.JPG.jpeg`,
   stats: [
